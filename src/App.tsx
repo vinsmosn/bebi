80b736a0c73eb03dc8159de99232f132329af674
vinsmosn <seanjevmed12@gmail.com>
2026-02-06 17:31:44 +0800

    fix: change base path for vercel FINAL

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,8 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">MWHIHIHI I LOVE YOU SO MUCHHHH, MY AJJI ;)) !!!</div>
+          <div className="my-4 text-4xl font-bold text-pink-600 font-serif">I LOVE YOU SO MUCH, MY AJJI ;)) !!!
+</div>
         </>
       ) : (
         <>
@@ -49,7 +50,7 @@ export default function Page() {
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="my-4 text-4xl">Avryle, can I be your Valentine?</h1>
+          <h1 className="my-4 text-4xl font-cursive">Avryle, can I be your Valentine?</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+      extend: {
+    fontFamily: {
+      cursive: ['Pacifico', 'cursive'],
+    },
+  },
     extend: {},
   },
   plugins: [],
